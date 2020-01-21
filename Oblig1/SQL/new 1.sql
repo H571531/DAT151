2ce2df7da251c3e4401a3b6dbd02bb361af6c322
H571531 <31210153+H571531@users.noreply.github.com>
2020-01-21 12:44:37 +0100

    Oblig1 Creation

---
+++ Oblig1/SQL/new 1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE bedrift(
+	bedriftsId varchar(4),
+	navn varchar(8),
+	CONSTRAINT bedriftPK PRIMARY KEY (bedriftsId)
+);
+
+CREATE TABLE person(
+	enId varchar(4),
+	navn varchar(8),
+	alder int,
+	bedriftsId varchar(4),
+	CONSTRAINT bedriftFK FOREIGN KEY (bedriftsId) REFERENCES bedrift(bedriftsId)
+	CONSTRAINT personPK PRIMARY KEY (enId)
+);
+
+
+
