3fd0416f27a3635f9aa210b3e2a2e35fd7eb0e20
Adrian <H571531@users.noreply.github.com>
2020-02-10 15:33:19 +0100

    Push av commands (new1)

--- Oblig3/Task3/Task3Print.sql
+++ Oblig3/Task3/Task3Print.sql
@@ -1,6 +1,6 @@
-MariaDB [privBase]> INSERT INTO teacher (name,salary,bonus) values ('Adrian',100,200);
+> INSERT INTO teacher (name,salary,bonus) values ('Adrian',100,200);
 ERROR 4025 (23000): CONSTRAINT `teacher.salary` failed for `privBase`.`teacher`
-MariaDB [privBase]> INSERT INTO teacher (name,salary,bonus) values ('Adrian',1001,200);
+> INSERT INTO teacher (name,salary,bonus) values ('Adrian',1001,200);
 Query OK, 1 row affected (0.004 sec)
 
 MariaDB [privBase]> select * from teacher;
