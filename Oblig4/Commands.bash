adca5d940d742d5b586429027144a0ce624e8ad9
Adrian <31210153+H571531@users.noreply.github.com>
2020-02-14 19:45:58 +0100

    Oblig4 complete

--- Oblig4/Commands.bash
+++ Oblig4/Commands.bash
@@ -1,4 +1,4 @@
-
+##Running this example on 2 raspberry pi micro computers running a debian based OS (Raspbian) 
 ## Server side
 $ sudo apt-get install nfs-kernel-server -y
 $ sudo chown -R pi:pi /home/
@@ -41,3 +41,44 @@ pi@AdrianDC:~ $ ls -al | grep Hei.txt
 -rw-r--r--  1 pi pi    0 feb.   7 10:57 Hei.txt
 
 
+##LVM
+$sudo pvcreate /dev/sdb1 /dev/sdb2 /dev/sdb3 /dev/sdb4
+$sudo vgdisplay
+#shows each of the partitions as separate units. 
+
+$sudo vgcreate vGroup /dev/sdb1 /dev/sdb2 /dev/sdb3 /dev/sdb4
+ Volume group "vGroup" successfully created
+ 
+ 
+##Logging
+
+#Local logging
+$sudo nano /etc/rsyslog.conf
+
+#added this line:
+auth.debug /var/log/auth.log
+#This adds the logs from the authentication process like login. 
+
+#uncommented these lines to allow for connection
+module(load="imudp")
+input(type="imudp" port="514")
+module(load="imtcp")
+input(type="imtcp" port="514")
+
+#then I did a restart of the service
+$sudo systemctl restart rsyslog
+
+
+
+$sudo nano /etc/rsyslog.conf
+#Added this to the bottom of the file (10.0.0.2 is the ip of the server)
+*.* @@10.0.0.2:514
+$sudo nano /etc/rsyslog.d/10-rsyslog.conf
+#Added this to the file I created:
+*.* @@10.0.0.2:514
+#restarting server
+$sudo systemctl restart rsyslog
+
+Now adding this to the client will send the logs for authentication to the server:
+
+auth.* @@10.0.0.2:514
