6a3700d1ead5879d8bf562b11ddfe44b5c4df67b
Adrian <H571531@users.noreply.github.com>
2020-02-25 13:53:25 +0100

    Push updated store procedure

--- Oblig3/Task6/createStoreProcedure.sql
+++ Oblig3/Task6/createStoreProcedure.sql
@@ -7,3 +7,12 @@ CREATE PROCEDURE takeSpace (pId VARCHAR(7),eventId VARCHAR(10),lName VARCHAR (15
 		END IF;	
 	END$
 delimiter ;
+
+delimiter $
+CREATE PROCEDURE takeSpace (pId VARCHAR(18),eventId VARCHAR(5))
+	BEGIN
+		IF (Select count(*) from Reservation where Reservation.eventId=eventId FOR UPDATE) < (SELECT totSpaces FROM Event WHERE Event.eventId=eventId) THEN
+            INSERT INTO Reservation (eventId,pId) VALUES (eventId, pId);
+		END IF;	
+	END$
+delimiter ;
