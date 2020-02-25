b493b29b634eb55d3e12839dc5e6c2d39e79ddf1
Adrian Mortensen <admo@Server65.domene65.driftslab.local>
2020-02-25 13:50:50 -0500

    Oblig3 updatesql

--- Oblig3/Task6/moveEventData.sql
+++ Oblig3/Task6/moveEventData.sql
@@ -1,3 +1,11 @@
 INSERT INTO Event
 SELECT DISTINCT eventId,eventTitle,eventDate,totSpaces 
 FROM auxTable;
+
+INSERT INTO Atendee
+SELECT DISTINCT pId,lName,fName
+FROM auxTable;
+
+INSERT INTO Reservation (pId,eventId)
+SELECT pId,eventId 
+FROM auxTable
